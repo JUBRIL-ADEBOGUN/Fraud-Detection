ee1289a03ce6ecbbc46176c9965757c01670fc67
JUBRIL ADEBOGUN <37392543+JUBRIL-ADEBOGUN@users.noreply.github.com>
2026-06-06 12:00:49 +0100

    Refactor imports and update sys.path insertion

--- src/serve.py
+++ src/serve.py
@@ -4,14 +4,14 @@ import pandas as pd # pyright: ignore
 import mlflow.sklearn # pyright: ignore
 import numpy as np # pyright: ignore
 import sys
-from pathlib import Path
-sys.path.insert(0, './src')
+
 from features import (
     XentePreprocessor
-)  # noqa: F401 - needed for model unpickling
+)  # needed for model unpickling
 
 # Set MLflow tracking URI at module level
 # mlflow.set_tracking_uri(Path.cwd() / "mlruns")
+sys.path.insert(0, './src')
 
 class XenteTransaction(BaseModel):
     TransactionId: str
